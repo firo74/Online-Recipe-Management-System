0a9dbfb9e173fbb109971d9c71a3a3d6a7e96e4a
firo74 <firo744@gmail.com>
2025-07-13 13:35:42 +0330

    Add view forms for creating recipe, category and ingredients

--- recipe/urls.py
+++ recipe/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
-from .views import RecipeListView, RecipeDetailView
+from .views import RecipeListView, RecipeDetailView, RecipeFormView, CategoryFormView, IngredientsFormView
 
 
 urlpatterns = [
-    path('recipe-list/', view=RecipeListView, name= 'recipe_list'),
-    path('recipe-details/<int:pk>', view= RecipeDetailView, name= 'recipe_details'),
-    
+    path('recipe-list/', RecipeListView, name= 'recipe_list'),
+    path('recipe-details/<int:pk>', RecipeDetailView, name= 'recipe_details'),
+    path('recipe-form/', RecipeFormView, name= 'recipe_form'),
+    path('category-form/', CategoryFormView, name= 'category_form'),
+    path('ingredients-form/', IngredientsFormView, name= 'ingredients_form'),
 
 ]
--- recipe/views.py
+++ recipe/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Recipe
+from .forms import RecipeForm, CategoryForm, IngredientsForm
 
 
 def RecipeListView(request):
@@ -10,3 +11,42 @@ def RecipeListView(request):
 def RecipeDetailView(request, pk):
     recipe = Recipe.objects.get(pk = pk)
     return render(request, 'recipe_details.html', {'recipe': recipe})
+
+
+def RecipeFormView(request):
+    if request.method == 'GET':
+        form = RecipeForm()
+        return render(request, 'recipe_form.html', {'form': form})
+    
+    elif request.method == 'POST':
+        form = RecipeForm(request.POST)
+        if form.is_valid():
+            form.save()
+            msg = 'Recipe is added successfully'
+            return render(request, 'recipe_form.html', {'form': form, 'msg': msg})
+            # return redirect('recipe_form')
+
+def CategoryFormView(request):
+    if request.method == 'GET':
+        form = CategoryForm()
+        return render(request, 'category_form.html', {'form': form})
+    
+    elif request.method == 'POST':
+        form = CategoryForm(request.POST)
+        if form.is_valid():
+            form.save()
+            msg = 'Category is added successfully'
+            return render(request, 'category_form.html', {'form': form, 'msg': msg})
+        
+
+def IngredientsFormView(request):
+    if request.method == 'GET':
+        form = IngredientsForm()
+        return render(request, 'ingredients_form.html', {'form': form})
+
+    elif request.method == 'POST':
+        form = IngredientsForm(request.POST)
+        if form.is_valid():
+            form.save()
+            msg = 'Ingredient is added successfully'
+            return render(request, 'ingredients_form.html', {'form': form, 'msg': msg})
