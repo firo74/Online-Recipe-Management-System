f09d5299d9f0d7c185a7cd162f1d1d3ef9238f96
Firooz Hosseini <firo744@gmail.com>
2025-07-16 11:06:47 +0330

    add new views and templates

--- recipe/urls.py
+++ recipe/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import RecipeListView, RecipeDetailView, RecipeFormView, CategoryFormView, IngredientsFormView, RecipeUpdateView
+from .views import RecipeListView, RecipeDetailView, RecipeFormView, CategoryFormView, IngredientsFormView, RecipeUpdateView, recipe_delete, recipes_by_category, recently_viewed
 
 
 urlpatterns = [
@@ -8,6 +8,9 @@ urlpatterns = [
     path('recipe-form/', RecipeFormView, name= 'recipe_form'),
     path('category-form/', CategoryFormView, name= 'category_form'),
     path('ingredients-form/', IngredientsFormView, name= 'ingredients_form'),
-    path('recipe-update/<int:pk>', RecipeUpdateView, name= 'recipe_update')
+    path('recipe-update/<int:pk>', RecipeUpdateView, name= 'recipe_update'),
+    path('recipe/<int:pk>/delete/', recipe_delete, name='recipe_delete'),
+    path('category/<int:category_id>/', recipes_by_category, name='recipes_by_category'),
+    path('recent/', recently_viewed, name='recently_viewed'),
 
 ]
--- recipe/views.py
+++ recipe/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, redirect
-from .models import Recipe
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Recipe, Category
 from .forms import RecipeForm, CategoryForm, IngredientsForm
+from django.http import HttpResponse
 
 
 def RecipeListView(request):
@@ -64,3 +65,28 @@ def RecipeUpdateView(request, pk):
             form.save()
             return redirect('recipe_list')
             # return render(request, 'recipe_update.html',{'form': form})
+
+
+
+def recipe_delete(request, pk):
+    recipe = get_object_or_404(Recipe, pk=pk)
+    if request.method == 'POST':
+        recipe.delete()
+        return redirect('recipe_list')
+    return render(request, 'recipe_confirm_delete.html', {'recipe': recipe})
+
+
+def recipes_by_category(request, category_id):
+    category = get_object_or_404(Category, id=category_id)
+    recipes = Recipe.objects.filter(categories=category)
+    return render(request, 'recipe_list.html', {'recipes': recipes, 'category': category})
+
+
+
+def recently_viewed(request):
+    ids = request.COOKIES.get('recently_viewed', '')
+    ids = ids.split(',') if ids else []
+    recipes = Recipe.objects.filter(pk__in=ids)
+    return render(request, 'recently_viewed.html', {'recipes': recipes})
+
+
